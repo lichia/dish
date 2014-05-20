81c7174c81441ceb358a48b37ceed547405720d2
James Porter <porterjamesj@gmail.com>
2014-05-20 17:32:47 -0500

    improve docstrings and rst-ify.

--- dish/pipeline.py
+++ dish/pipeline.py
@@ -26,15 +26,19 @@ class Pipeline(object):
                  local=False):
         """Initialize a pipeline.
 
-        workdir: directory to use for scratch space and results. this
-        needs to be visible to all nodes over NFS or similar
+        :param workdir: Name of a directory to use for scratch space
+        and results. This needs to be visible to all nodes over NFS or
+        similar.
 
-        jobs: a list of jobs, which are just dicts. the only required
-        key for now is "description", which will be used for the
-        directory that holds all this job's output
+        :param jobs: A list of jobs, which are just dicts. The only
+        required key for now is "description", which will be used for
+        the directory that holds all this job's output.
 
-        total_cores: the total number of cores you have available for
-        processing
+        :param total_cores: The total number of cores you want to use
+        for processing.
+
+        :returns: A Pipeline object, which has methods that invoke
+        various kinds of distributed work.
 
         """
 
@@ -56,7 +60,6 @@ class Pipeline(object):
         self.local = local
         # setup default cluster_view
         self._cluster_view = cluster_view
-
 
     def start(self):
         """Initialize workdir, logging, etc. in preparation for running jobs.
@@ -114,6 +117,20 @@ class Pipeline(object):
 
     @contextmanager
     def group(self, cores=1, mem=None, max=None):
+        """Context manager for "grouping" a set of pipeline operations. A
+        group of operations is run on the same ipython cluster and has
+        it's resources specified in the group as opposed to in each
+        individual job. This is useful if there is some small amount
+        of setup work that isn't worth spinning up a new cluster for
+        but which needs to be done before a resource intensive task.
+
+        For example::
+
+            with p.group(cores=8, mem=12):
+               p.run("setup.sh . . .")  # do some data munging or other setup
+               p.run("main_work -n 8 . . .")  # call an expensive program
+
+        """
         # TODO this duplicates some code from p.map and is a bit
         # clunky, there is probably a better abstraction here
         engines, cores, mem = self._compute_resources(cores, mem, max)
@@ -144,6 +161,53 @@ class Pipeline(object):
 
     @contextmanager
     def transaction(self, targets):
+        """Do some work "transacationally", in the sense that nothing done
+        inside a ``transaction`` block will be "commited" to the
+        workdir unless it all succeeds without error. The work done
+        inside a transaction is also idempotent in that you must
+        specify a ``target`` file or files for the tranasaction and it
+        will not be run if the target exists already. This is perhaps
+        best illustrated by a simple example::
+
+            with p.transaction("{workdir}/example.txt"):
+                p.run("{tmpdir}/touch example.txt")
+
+        This will result in a file ``B.txt`` in each job's
+        ``workdir``. The creation of this file will be skipped if the
+        code is run again and the file already exists. This is
+        obviously a silly example, but the code inside the `with`
+        block can be any arbitrarily complex series of operations
+        which produces a set of target output files at the end. This
+        is a powerful feature in that it allows pipelines to be
+        restratable: if a pipeline crashes for some reason but you
+        have it's major sections wrapped in ``transaction`` blocks,
+        you can simple run it again and pick up where you left off
+        without redoing any work. The transaction blocks guarentee
+        that the ``workdir`` for each job is never in an inconsistent
+        state and that work that's already been completed isn't
+        redone.
+
+        Inside a transaction, each job has a special ``tmpdir`` key,
+        whose value is the path to a unique temporary directory for
+        the job. You can do work that produces files inside the
+        ``tmpdir`` and expect everything in it to be moved to the
+        job's ``workdir`` if the transaction compeltes without error.
+        The ``tmpdir`` will be removed at the end of the transaction
+        regardless of whether or not it succeeds. We change
+        directories to the ``tmpdir`` before doing anything else and
+        implicitly consider targets to be relative to a job's
+        ``workdir`` so the above example could also be written
+        written::
+
+            with p.transaction("example.txt"):
+                p.run("touch example.txt")
+
+        which sacrifices explicitness for brevity.
+
+        :param targets: a string or list of strings descsribing files
+        that must exist in order for the transaction to be skipped.
+
+        """
         if type(targets) is not list:
             targets = [targets]
         to_run = []
@@ -174,9 +238,11 @@ class Pipeline(object):
             self.jobs = dont_run + self.jobs
 
     def localmap(self, f):
-        """Just like map, but work locally rather than launching an ipython
+        """Just like ``map``, but work locally rather than launching an ipython
         cluster.  This is useful for tasks where the cluster launch
         overhead would swamp the cost of the actual work to be done.
+
+        :params f: function of ``(job, logger)`` to be mapped over all jobs.
 
         """
         self.jobs = map(logging_wrapper, self.jobs,
@@ -185,27 +251,30 @@ class Pipeline(object):
                         (self.listen_port for j in self.jobs))
 
     def map(self, f, cores=1, mem=None, max=None):
-        """Map the function `f` over all of the `jobs` in this pipeline. `f`
-        must be a function of two arguments, the job and a logger. It
-        should modify the job it is passed, which will then be
-        returned over the wire. A silly example:
+        """Map the function ``f`` over all of the ``jobs`` in this
+        pipeline. ``f`` must be a function of two arguments, the job
+        and a logger. It should modify the job it is passed, which
+        will then be returned over the wire. A silly example::
 
-        ```
-        def f(job, logger):
-            job["capitalized_description"] = job["description"].toupper()
-        p.map(f)
-        ```
+            def f(job, logger):
+                job["capitalized_description"] = job["description"].toupper()
+            p.map(f)
 
-        Will give each `job` in the pipeline a `capitalized_description`
+        Will give each ``job`` in the pipeline a ``capitalized_description``
         attribute, which can then be used in future pipline operations.
 
-        `cores` and `mem` are used to specify the cores and memory
+        ``cores`` and ``mem`` are used to specify the cores and memory
         required by this step; they will be passed to the underlying
-        scheduler. `max` can be used as a hard limit on the number of
+        scheduler. ``max`` can be used as a hard limit on the number of
         jobs to run. This is useful if, for example, a particular task
         puts pressure on some sort of storage system (a distributed
         file system, object store, etc.) that you know will fail under
         too much load.
+
+        :param f: function of ``(job, logger)`` to be mapped over all jobs.
+        :param cores: cores required by this call.
+        :param mem: memory required by this call.
+        :param max: maximum number of jobs to submit.
 
         """
         if not self.jobs:
@@ -233,18 +302,18 @@ class Pipeline(object):
                                       (self.listen_port for j in self.jobs))
 
     def run(self, template, capture_in=None, **kwargs):
-        """Run the `template` formatted with the contents of each job. Example:
+        """Run the ``template`` formatted with the contents of each
+        job. Example::
 
-        ```
-        p.run("touch {workdir}/example.txt")
-        ```
+            p.run("touch {workdir}/example.txt")
 
         will make an example.txt file in each job's workdir.
 
-        `cores` and `mem` mean the same thing they do in the `map` method.
+        ``cores`` and ``mem`` mean the same thing they do in the
+        ``map`` method.
 
-        If a string is passed for `capture_in`, the stdout of the command
-        will be captured in `job[capture_in]` for each job.
+        If a string is passed for ``capture_in``, the stdout of the command
+        will be captured in ``job[capture_in]`` for each job.
 
         """
         runner = cmdrunner(template, capture_in)
